995e6e371b5c21557029c120a15feeba901caa77
Nikkex1 <niklas.pulliainen@gmail.com>
2025-07-26 18:16:06 +0300

    Fetch rates from 1999 onwards

--- ir_prediction/interest_rates.py
+++ ir_prediction/interest_rates.py
@@ -76,6 +76,45 @@ class Euribor():
         df[1] = r_column
 
         return df
+    
+    def fetch_by_year(self, y: int = 2025):
+        """Returns the monthly rates of a given year for the given maturity."""
+
+        # Construct the url:
+        d = {"1 month": 1,
+             "3 months": 2,
+             "6 months": 3,
+             "12 months": 4,
+             "1 week": 5}
+        m_id = d[self.__maturity]
+        url = f"https://www.global-rates.com/en/interest-rates/euribor/historical/{y}/?id={m_id}#bmrk-maturity"
+
+        # Monthly rates are in the second table
+        df = pd.read_html(url)[1]
+
+        # Use the first day of the month, drop everything else
+        df.drop(columns=["Unnamed: 0","Last","Highest","Lowest","Average"],
+                inplace=True)
+        
+        #Rename the remaining column for simplicity
+        df.rename(columns={"First": 0},
+                  inplace=True)
+        
+        # Monthly date index, first business day of the month
+        idx = pd.date_range(f"{y}/1/1",f"{y}/12/31",freq="BMS")
+        df.index = idx
+        df.index.name = "Date"
+
+        # Check for empty values and drop them
+        mask = df[0] == "-"
+        df[mask] = np.nan
+        df.dropna(inplace=True)
+
+        # Convert the string rates ("X.xx %") to float (0.0Xxx)
+        r_column = (np.char.replace(list(df[0])," %","").astype(float)) / 100
+        df[0] = r_column
+
+        return df
 
 if __name__ == "__main__":
     maturities = ["1 week", #0
@@ -84,6 +123,5 @@ if __name__ == "__main__":
                   "6 months", #3
                   "12 months"] #4
 
-    r = Euribor(maturities[4])
-
-    print(r.get_current())
+    r = Euribor()
+    print(r.fetch_by_year())
