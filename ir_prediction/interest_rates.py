3c4a6f908584ce23fc490d1086499f3a1a70c26d
Nikkex1 <niklas.pulliainen@gmail.com>
2025-07-28 14:07:53 +0300

    Standardize web scraping methods

--- ir_prediction/interest_rates.py
+++ ir_prediction/interest_rates.py
@@ -1,65 +1,36 @@
 import pandas as pd
 import numpy as np
+from typing import Literal
 
 class Euribor():
 
-    def __init__(self, maturity: str = "3 months"):
+    def __init__(self, maturity: Literal["1 week",
+                                         "1 month",
+                                         "3 months",
+                                         "6 months",
+                                         "12 months"]):
         """
         Fetch Euribor rates of a given maturity.
 
         Parameters
         ----------
+        maturity: `str`
+            The Euribor's maturity
         """
 
         self.__maturity = maturity
 
     def get_current(self):
-        """Return the latest daily Euribor rate."""
+        """Returns the Euribor rate of the previous business day."""
 
         return self.get_daily().iloc[0,0]
 
     def get_daily(self):
-        """Returns the current Euribor rates from the last 10 days."""
+        """Returns the daily Euribor rates from the last 10 days."""
 
-        df = self.__construct(by=0)
-        df.rename(columns={1: f"Daily rates ({self.__maturity})"},
+        df = self.__fetch_by_day()
+        df.rename(columns={1: f"Euribor ({self.__maturity})"},
                   inplace=True)
-
-        return df
-    
-    def __fetch(self):
-        """Returns a list of Euribors by day, month and year"""
-
-        # For constructing the url below
-        d = {"1 month": 1,
-             "3 months": 2,
-             "6 months": 3,
-             "12 months": 4,
-             "1 week": 5}
-        
-        m_number = d[self.__maturity]
-        m_label = self.__maturity.split()
-
-        url = f"https://www.euribor-rates.eu/en/current-euribor-rates/{m_number}/euribor-rate-{m_label[0]}-{m_label[1]}/"
-
-        euribors = pd.read_html(url)
-
-        # day [0], month [1], year [2] of given maturity
-        return euribors
-    
-    def __construct(self, by: int):
-        """Returns a cleaned DataFrame of the rates by day, month or year."""
-        
-        df = self.__fetch()[by]
-
-        # Assign date index and drop the old date column
-        df.index = df[0].astype("datetime64[ns]")
-        df.index.name = "Date"
-        df.drop(columns=0,inplace=True)
-
-        # Convert the string rates to float
-        r_column = (np.char.replace(list(df[1])," %","").astype(float)) / 100
-        df[1] = r_column
 
         return df
     
@@ -72,6 +43,7 @@ class Euribor():
 
         df = self.__concat(s,e)
         mask = df.index.month == 1
+        df.rename(columns={0:f"Euribor ({self.__maturity})"},inplace=True)
 
         return df[mask]
     
@@ -98,6 +70,34 @@ class Euribor():
 
         return pd.concat(df_list)
     
+    def __fetch_by_day(self):
+        """Returns the cleaned daily rates from the last 10 days for the given maturity."""
+
+        # Construct the url
+        d = {"1 month": 1,
+             "3 months": 2,
+             "6 months": 3,
+             "12 months": 4,
+             "1 week": 5}
+        m_id = d[self.__maturity]
+        m_label = self.__maturity.split()
+
+        url = f"https://www.euribor-rates.eu/en/current-euribor-rates/{m_id}/euribor-rate-{m_label[0]}-{m_label[1]}/"
+
+        # Daily Euribors are in the first table
+        df = pd.read_html(url)[0]
+
+        # Assign date index and drop the old date column
+        df.index = df[0].astype("datetime64[ns]")
+        df.index.name = "Date"
+        df.drop(columns=0,inplace=True)
+
+        # Convert the string rates ("X.xxx %") to float (0.0Xxxx)
+        r_column = (np.char.replace(list(df[1])," %","").astype(float)) / 100
+        df[1] = r_column.round(5) # round to avoid long decimals due to python miscalculations
+
+        return df 
+    
     def __fetch_by_year(self, y: int):
         """Returns the cleaned monthly rates of a given year for the given maturity."""
 
@@ -122,7 +122,7 @@ class Euribor():
         df.drop(columns=["Unnamed: 0","Last","Highest","Lowest","Average"],
                 inplace=True)
         
-        #Rename the remaining column for simplicity
+        #Rename the remaining column to 0 for simplicity
         df.rename(columns={"First": 0},
                   inplace=True)
         
@@ -131,25 +131,22 @@ class Euribor():
         df.index = idx
         df.index.name = "Date"
 
-        # Check for empty values and drop them
+        # Check for empty values (months of current year yet to come) and drop them
         mask = df[0] == "-"
         df[mask] = np.nan
         df.dropna(inplace=True)
 
-        # Convert the string rates ("X.xx %") to float (0.0Xxx)
+        # Convert the string rates ("X.xxx %") to float (0.0Xxxx)
         r_column = (np.char.replace(list(df[0])," %","").astype(float)) / 100
-        df[0] = r_column.round(5) # round to avoid errors due to python miscalculations
+        df[0] = r_column.round(5) # round to avoid long decimals due to python miscalculations
 
         return df
 
 if __name__ == "__main__":
-    maturities = ["1 week", #0
-                  "1 month", #1
-                  "3 months", #2
-                  "6 months", #3
-                  "12 months"] #4
 
-    r = Euribor()
+    r = Euribor("12 months")
     #print(r.get_monthly("1999","2025/07"))
 
-    print(r.get_yearly("1999/01/11","2025/07/31"))
+    #print(r.get_yearly("1999/01/11","2025/07/31"))
+    print(r.get_daily())
+    print(r.get_current())
