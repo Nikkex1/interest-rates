3a0481773e368659d88d23cd47b2893b635e16f0
Nikkex1 <niklas.pulliainen@gmail.com>
2025-07-29 15:43:24 +0300

    Add results and stats methods

--- ir_prediction/monte_carlo.py
+++ ir_prediction/monte_carlo.py
@@ -4,67 +4,108 @@ import matplotlib.pyplot as plt
 from ir_models import *
 
 class MonteCarlo():
-    """Monte Carlo simulation"""
 
-    def __init__(self, model: VasicekModel | CIRModel):
-        """init"""
+    def __init__(self, model: VasicekModel | CIRModel, number_of_simulations: int):
+        """
+        Monte Carlo simulation for Vasicek and Cox-Ingersoll-Ross models.
+        
+        Parameters
+        ----------
+        model: `VasicekModel` | `CIRModel`
+            The interest rate model to simulate
+        number_of_simulations: `int`
+            The number of Monte Carlo simulations to run
+        """
 
         self.__model = model
+        self.__nsims = number_of_simulations
         self.__params = model.get_params()
 
-        # initialize the variable to prevent rerunning the simulation
-        self.__all_sims = 0
-
-    def run(self,number_of_simulations: int):
-        """Run the Monte Carlo simulation."""
-
-        # N rows (time steps)
-        # number_of_simulation columns (1 simulation per 1 column)
+        # N rows, number_of_simulation columns
         self.__all_sims = np.zeros((self.__params["N"],number_of_simulations))
+        self.__run(number_of_simulations)
 
-        for i in range(number_of_simulations):
-            self.__all_sims[:,i] = self.__model.get_rates()
+
+    # Methods for showing the results, summary statistics and visualization
 
     def results(self):
-        """Return the Monte Carlo simulation results as a DataFrame."""
+        """
+        Returns the Monte Carlo simulation results as a DataFrame.
+        
+        The results for each simulation run are in their respective columns.
+        """
 
-        if type(self.__all_sims) == int:
-            return None
-        else:
-            df = pd.DataFrame(self.__all_sims)
-            df.index.name = "Time step"
-            df.columns = [f"Simulation {i}" for i in range(1,len(df.columns) + 1)]
-            return df
+        df = pd.DataFrame(self.__all_sims)
+        df.index.name = "Time step"
+        df.columns = [f"Simulation {i}" for i in range(1,len(df.columns) + 1)]
+        return df
+    
+    def stats(self):
+        """Returns summary statistics of the simulation results."""
+
+        qs = self.__quantiles()
+        q1 = qs["q1"].iloc[-1]
+        q5 = qs["q5"].iloc[-1]
+        q9 = qs["q9"].iloc[-1]
+
+        labels = ["Initial Interest Rate (r0)",
+                  "Long-Term Mean (mu)",
+                  "90% quantile",
+                  "50% quantile",
+                  "10% quantile"]
+        
+        data = [self.__params["r0"],
+                self.__params["mu"],
+                q9,
+                q5,
+                q1]
+
+        df = pd.DataFrame(data=data,
+                          index=labels,
+                          columns=[f"Results for {self.__nsims} simulations"])
+
+        return df
         
     def visualize(self):
         """Visualizes the simulated interest rate paths."""
 
         df = self.results()
+        q = self.__quantiles()
 
         fig,ax = plt.subplots()
         ax.set_xlim([0,self.__params["N"]])
 
         for c in df.columns:
-            plt.plot(df[c])
+            plt.plot(df[c],linewidth=0.5)
 
+        plt.plot(q["q9"],"--",color="0.3",label=".9 quantile")
+        plt.plot(q["q5"],"k--",label=".5 quantile")
+        plt.plot(q["q1"],"--",color="0.3",label=".1 quantile")
+
+        plt.legend()
         plt.show()
 
+    
+    # Hidden methods for calculations
+
+    def __run(self, n: int):
+        """Run the Monte Carlo simulation."""
+
+        for i in range(n):
+            self.__all_sims[:,i] = self.__model.get_rates()
+
+    def __quantiles(self):
+        """Returns the .1, .5 and .9 quantiles of the simulation"""
+
+        df = self.results()
+
+        q1 = df.quantile(q=0.1,axis=1)
+        q5 = df.quantile(axis=1) #default 0.5
+        q9 = df.quantile(q=0.9,axis=1)
+
+        quantiles = pd.DataFrame({"q1":q1,"q5":q5,"q9":q9})
+
+        return quantiles
+
 if __name__ == "__main__":
-    theta = 2
-    mu = 0.05
-    sigma = 0.02
-    r0 = 0.03
-
-    v_model = VasicekModel(theta,mu,sigma,r0)
-    v_monte_carlo = MonteCarlo(v_model)
-
-    v_monte_carlo.run(100)
-    print(v_monte_carlo.results())
-
-    cir_m = CIRModel(theta,mu,sigma,r0)
-    cir_monte_carlo = MonteCarlo(cir_m)
-
-    cir_monte_carlo.run(100)
-    print(cir_monte_carlo.results())
-
-    v_monte_carlo.visualize()
+    pass
